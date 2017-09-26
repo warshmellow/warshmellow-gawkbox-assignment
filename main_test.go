30e9f19119be3325be9a446950db8e4eaba3dee2
warshmellow <ddwchensf@gmail.com>
2017-09-26 10:28:48 -0700

    Refactor http tests with test doubles

--- main_test.go
+++ main_test.go
@@ -7,13 +7,48 @@ import (
 	"testing"
 )
 
+var mux *http.ServeMux
+
+type TestTwitchy struct{}
+
+func (t TestTwitchy) GetChannel(id int) twitch.GetChannelResponse {
+	return twitch.GetChannelResponse{
+		ID:        id,
+		Followers: 333,
+		Game:      "Dark Souls 3",
+		Language:  "en",
+		Views:     9001,
+	}
+}
+
+func (t TestTwitchy) GetStream(id int) twitch.GetStreamResponse {
+	return twitch.GetStreamResponse{ID: id, StreamingNow: true}
+}
+
+func (t TestTwitchy) GetUser(id int) twitch.GetUserResponse {
+	return twitch.GetUserResponse{
+		ID:          id,
+		Bio:         "Just a gamer playing games and chatting. :)",
+		CreatedAt:   "2013-06-03T19:12:02Z",
+		DisplayName: "dallas",
+	}
+}
+
+func init() {
+	mux = http.NewServeMux()
+
+	testTwitchy := TestTwitchy{}
+
+	mux.HandleFunc("/channels", handleGetChannel(testTwitchy))
+	mux.HandleFunc("/streams", handleGetStream(testTwitchy))
+	mux.HandleFunc("/users", handleGetUser(testTwitchy))
+}
+
 func TestGetChannelsHandler(t *testing.T) {
 	req := httptest.NewRequest("GET", "/channels?id=1", nil)
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleGetChannel(twitch.TwitchAPI{}))
-
-	handler.ServeHTTP(rr, req)
+	mux.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
@@ -31,9 +66,7 @@ func TestGetStreamHandler(t *testing.T) {
 	req := httptest.NewRequest("GET", "/streams?id=1", nil)
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleGetStream(twitch.TwitchAPI{}))
-
-	handler.ServeHTTP(rr, req)
+	mux.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
@@ -51,9 +84,7 @@ func TestGetUserHandler(t *testing.T) {
 	req := httptest.NewRequest("GET", "/users?id=1", nil)
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleGetUser(twitch.TwitchAPI{}))
-
-	handler.ServeHTTP(rr, req)
+	mux.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
